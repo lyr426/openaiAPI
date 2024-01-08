bef774bda00142adabc1f7ee534f19819ab17ada
lyr426 <leeyr426@gmail.com>
2024-01-08 14:33:57 +0900

    이미지 첨부 기능

--- commands/utility/imageGenerate.js
+++ commands/utility/imageGenerate.js
@@ -1,22 +1,19 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const request = require('request-promise'); 
-const wait = require('node:timers/promises').setTimeout;
 require('dotenv').config();
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('번역')
-        .setDescription('한글을 영어로 번역해주는 기능')
+		.setName('이미지생성')
+        .setDescription('입력한 문장으로 이미지를 생성해주는 기능')
         .addStringOption(option => 
             option
                 .setName('text')
-                .setDescription('번역할 문장을 입력')
+                .setDescription('생성할 이미지에 대한 설명을 입력')
                 .setRequired(true)),
 	async execute(interaction) {
         const text = interaction.options.getString('text') ?? '문장은 필수 입력'; 
-        //await interaction.deferReply();
-        //const translated_text = translate(text); 
-        // await wait(5_000);
+        const translated_text = ''; 
         const client_id = process.env.NAVER_CLIENT_ID;
         const client_secret = process.env.NAVER_CLIENT_SECRET;  
         const api_url = 'https://openapi.naver.com/v1/papago/n2mt';
@@ -29,13 +26,17 @@ module.exports = {
          try{
             const response = await request.post(options); 
             // var translatedText = response.message.result;
-            const translated_text = JSON.parse(response).message.result.translatedText;
+            translated_text = JSON.parse(response).message.result.translatedText;
             console.log(translated_text);
             await interaction.reply("번역된 문장: " + translated_text);
          } catch (error) {
             console.error('Error:', error); 
-         }
+         }  
 
-		//await interaction.editReply(translated_text);
+        const exampleEmbed = new EmbedBuilder()
+            .setTitle(text)
+            .setImage('https://user-images.githubusercontent.com/56033943/217758183-6fb570ed-cd49-461a-a62b-124be2ab1a61.png');
+
+        await interaction.reply({ embeds: [exampleEmbed] });
 	},
 };
